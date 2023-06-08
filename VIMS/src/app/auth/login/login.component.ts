691778c8317d02168a5595ad91a5bd4cbb109202
Iftikhar Joyia <iftikharjoyia2324@gmail.com>
2023-06-08 14:01:50 +0500

    Login and register pages routing

--- VIMS/src/app/auth/login/login.component.ts
+++ VIMS/src/app/auth/login/login.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+/*tslint:disable*/
 
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string;
+  password: string;
 
+  constructor() {
+    this.email = '';
+    this.password = '';
+  }
+
+  login() {
+    // Implement your login logic here
+    console.log('Login button clicked');
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
